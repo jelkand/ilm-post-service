d85b8df56af2b9e3272e95c5bdc1edd76823ef42
Jack Anderson <andersjack@gmail.com>
2019-09-28 11:33:18 -0600

    sort out ports

--- src/server/server.ts
+++ src/server/server.ts
@@ -12,7 +12,7 @@ import resolvers from '../graphql/resolvers'
   })
 
   const app = express()
-  const port = process.env.port || 3000
+  const port = process.env.PORT || 3000
 
   server.applyMiddleware({ app })
 
--- src/typings/environment.d.ts
+++ src/typings/environment.d.ts
@@ -1,10 +1,11 @@
 import * as ts from 'typescript'
-import * as Bluebird from 'bluebird';
+import * as Bluebird from 'bluebird'
 
 declare global {
   interface Promise<T> extends Bluebird<T> {}
   namespace NodeJS {
     interface ProcessEnv {
+      PORT: string
       POSTGRES_DB: string
       POSTGRES_USER: string
       POSTGRES_PASSWORD: string
